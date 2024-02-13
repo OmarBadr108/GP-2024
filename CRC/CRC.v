b44921a4ab54413a26eca87e3aad50a8ebef4261
Omar Mohammed Badr <omerbader108@gmail.com>
2024-02-13 16:24:38 +0200

    initalization

---
+++ CRC/CRC.v
@@ -0,0 +1,31 @@
+/*//////////////////////////////////////////////////////////////////////////////////
+==================================================================================
+ MIXEL GP 2024 LIBRARY
+ Copyright (c) 2023 Mixel, Inc.  All Rights Reserved.
+ CONFIDENTIAL AND PROPRIETARY SOFTWARE/DATA OF MIXEL and ASU 2024 GP, INC.
+
+ Authors: 
+ 
+ Revision:   
+
+ Version : 1.0
+
+ Create Date: 
+ Design Name:  
+ Module Name:  
+
+==================================================================================
+
+  STATEMENT OF USE
+
+  This information contains confidential and proprietary information of MIXEL.
+  No part of this information may be reproduced, transmitted, transcribed,
+  stored in a retrieval system, or translated into any human or computer
+  language, in any form or by any means, electronic, mechanical, magnetic,
+  optical, chemical, manual, or otherwise, without the prior written permission
+  of MIXEL.  This information was prepared for Garduation Project purpose and is for
+  use by MIXEL Engineers only.  MIXEL and ASU 2023 GP reserves the right 
+  to make changes in the information at any time and without notice.
+
+==================================================================================
+//////////////////////////////////////////////////////////////////////////////////*/
