7431634b315f6910ede575c2ab08eac0fea23210
LailaaTamer <lilly.tw@hotmail.com>
2024-02-18 21:52:46 +0200

    Update ENTHDR_TB.v

--- ENTHDR/ENTHDR_TB.v
+++ ENTHDR/ENTHDR_TB.v
@@ -1,5 +1,37 @@
 `timescale 1us / 1ps
 
+/*//////////////////////////////////////////////////////////////////////////////////
+==================================================================================
+ MIXEL GP 2024 LIBRARY
+ Copyright (c) 2023 Mixel, Inc.  All Rights Reserved.
+ CONFIDENTIAL AND PROPRIETARY SOFTWARE/DATA OF MIXEL and ASU 2024 GP, INC.
+
+ Authors: Omar Maghraby, Laila Tamer 
+ 
+ Revision:   
+
+ Version : 1.0
+
+ Create Date: 
+ Design Name:  
+ Module Name:  
+
+==================================================================================
+
+  STATEMENT OF USE
+
+  This information contains confidential and proprietary information of MIXEL.
+  No part of this information may be reproduced, transmitted, transcribed,
+  stored in a retrieval system, or translated into any human or computer
+  language, in any form or by any means, electronic, mechanical, magnetic,
+  optical, chemical, manual, or otherwise, without the prior written permission
+  of MIXEL.  This information was prepared for Garduation Project purpose and is for
+  use by MIXEL Engineers only.  MIXEL and ASU 2023 GP reserves the right 
+  to make changes in the information at any time and without notice.
+
+==================================================================================
+//////////////////////////////////////////////////////////////////////////////////*/
+
 `default_nettype none
 module ENTHDR_tb();
 
