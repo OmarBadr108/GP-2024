f8124b7adb19cad5b5cb909e76e2a5242681268d
Omaghrabi26 <omaghrabi26@gmail.com>
2024-02-25 19:38:21 +0200

    Update Rx.v

--- Rx/Rx.v
+++ Rx/Rx.v
@@ -44,11 +44,11 @@ input     [2:0]           i_ddrccc_rx_mode,
 input                     i_crc_value,
 input                     i_crc_valid,
 
-output    [7:0]           o_regfcrc_rx_data_out,
-output                    o_ddrccc_rx_mode_done,
-output                    o_ddrccc_second_pre,
-output                    o_ddrccc_error,
-output                    o_crc_en                 
+output  reg  [7:0]        o_regfcrc_rx_data_out,
+output  reg               o_ddrccc_rx_mode_done,
+output  reg               o_ddrccc_second_pre,
+output  reg               o_ddrccc_error,
+output  reg               o_crc_en                 
 
 );
 
