9c5641e2116d1083b4fff4ae9f6b217f651a9ddc
LailaaTamer <lilly.tw@hotmail.com>
2024-02-29 00:40:16 +0200

    .

--- Rx/Rx.v
+++ Rx/Rx.v
@@ -159,6 +159,7 @@ begin
     o_ddrccc_error        <= 1'b0;
     o_crc_en              <= 1'b0; 
     //rx_mode_done_flag     <= 1'b0;
+    o_crc_data_valid      <= 1'b0;
    case(i_ddrccc_rx_mode) 
 
     PREAMBLE :          begin
@@ -305,7 +306,8 @@ begin
                  o_ddrccc_rx_mode_done <= 1'b0;
                  o_ddrccc_pre          <= 1'b0;
                  o_ddrccc_error        <= 1'b0;
-                 o_crc_en              <= 1'b0;         
+                 o_crc_en              <= 1'b0; 
+                 o_crc_data_valid      <= 1'b0;        
                  end
 
    
