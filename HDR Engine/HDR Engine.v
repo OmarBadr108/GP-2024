ffeb173633a368eab44b06508c654136d6df274e
FaToma245 <104271503+FaToma245@users.noreply.github.com>
2024-03-01 21:35:59 +0200

    .

--- HDR Engine/HDR Engine.v
+++ HDR Engine/HDR Engine.v
@@ -37,7 +37,6 @@ module hdr_engine (
     input   wire            i_ddr_mode_done                       ,
     input   wire            i_TOC                                 , //term of completion if 0 restart/ 1 exit needed for exit
     input   wire            i_CP                                  , // Cmnd present=1 if CCC 0 for Normal Transcation
-    //input   wire  [3:0]     i_TID                                 , //Transaction id for each CCC
     input   wire  [2:0]     i_MODE                                ,
     //to_blocks
     output  reg             o_i3cengine_hdrengine_done            ,
@@ -85,28 +84,28 @@ always @(posedge i_sys_clk or negedge i_sys_rst_n )
           CCC : begin
             if((i_TOC && i_ccc_done)||(i_MODE != 'd6)) begin
                   o_ccc_en    <= 1'b0 ;
-                  //ccc_done <= 1'b1 ;
                   o_i3cengine_hdrengine_done      <= 1'b1 ;
 
 
             end
             else if ((!i_TOC && i_ccc_done) && (i_MODE == 'd6)) begin
-              ccc_done   <= 1'b1 ;
+              ccc_done   <= 1'b1 ; //******signal 3mltha 3shan a3rf arg3 ll ddrmode*//////
               o_ccc_en   <= 1'b0 ;
+              o_regf_addr_special <= 8'd10;
                   if(!i_CP) 
                   begin
                     ccc_done <= 1'b0 ;
                     o_regf_addr_special <= 8'd9; //go to special address to get dummy value
                     o_ccc_en   <= 1'b1 ;
-                    next_state <= CCC ;
+                    next_state <= CCC ; ////********lma yru7 y3ml al dummy hwdeh ddr azay*******//////////
                   end
                   else
                     next_state <= CCC ;
-
-                  if(!i_CP && ccc_done)
+/**************guz2 da w ana bfkr azay awdeh ddrmode*///////////
+                  /*if(!i_CP && ccc_done)
                     next_state <= DDR_MODE ;
                   else
-                    o_i3cengine_hdrengine_done      <= 1'b1 ;
+                    o_i3cengine_hdrengine_done      <= 1'b1 ;*/
             end
             else
                   o_i3cengine_hdrengine_done      <= 1'b0 ;
@@ -117,18 +116,17 @@ always @(posedge i_sys_clk or negedge i_sys_rst_n )
               o_i3cengine_hdrengine_done      <= 1'b1 ;
             end
             else if ((!i_TOC && i_ccc_done) && (i_MODE == 'd6)) begin
-              if (!CP) begin
-                o_ddrmode_en <= 1'b1 ;
-                next_state   <= DDR_MODE ;
-              end
-              else begin
-                o_ccc_en <= 1'b1 ;
-                next_state <= CCC ;
-              end
-            end
+                  if (!CP) begin
+                    o_ddrmode_en <= 1'b1 ;
+                    next_state   <= DDR_MODE ;
+                  end
+                  else begin
+                    o_ccc_en <= 1'b1 ;
+                    next_state <= CCC ;
+                  end
+                  end
             else
               begin
-                o_ddrmode_en    <= 1'b1 ;
                 o_i3cengine_hdrengine_done      <= 1'b0 ;
               end
           end
